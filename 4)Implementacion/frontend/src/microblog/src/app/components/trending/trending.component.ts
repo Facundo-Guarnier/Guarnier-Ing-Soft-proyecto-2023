e9a89ff2927b289099fc93e0611d189d204effc2
Facundo-Guarnier <fj.guarnier@alumno.um.edu.ar>
2023-12-04 18:18:29 -0300

    Corrección cambiar días

--- 4)Implementacion/frontend/src/microblog/src/app/components/trending/trending.component.ts
+++ 4)Implementacion/frontend/src/microblog/src/app/components/trending/trending.component.ts
@@ -55,19 +55,26 @@ export class TrendingComponent implements OnInit {
       dias: [this.dias, Validators.required],
       }    
     )
-
   }
   
-  submit() {
-    this.DiasService.putDias({"dias":this.diasForm.value.dias}, localStorage.getItem("token")).subscribe(
-      (response) => {
-        alert(response);
-        window.location.reload();
-      }
-    )
-
-    this.HashtagTendenciaService.postHashtagTendencia(this.token).subscribe();
+  //! Cambiar días
+  actualizarDias() {
+    const dias = Number(this.diasForm.value.dias);
+    if (!isNaN(dias) && dias > 0) {
+      this.DiasService.putDias({"dias": dias}, localStorage.getItem("token")).subscribe(
+        (response) => {
+          alert(response);
+          window.location.reload();
+        }
+      );
+      
+      //! Enviar emails
+      this.HashtagTendenciaService.postHashtagTendencia(this.token).subscribe();
+    } else {
+      alert('El valor de días debe ser un número mayor a 0');
+    }
   }
+
 
   getDecodedAccessToken(token: any): any {
     try {
